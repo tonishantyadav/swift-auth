a6f7d87348185fc127d5400af104fa6bb316709d
Nishant Yadav <inishantyadav24@gmail.com>
2024-03-26 01:01:02 +0530

    Refactor: Fix layout issue of homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,50 +1,41 @@
-import { LoginButton } from '@/components/auth'
-import { Button, Card } from '@/components/ui'
-import { CardContent, CardHeader } from '@/components/ui/card'
+'use client'
+
+import { Button } from '@/components/ui'
 import shieldIcon from '@/public/shield.png'
 import Image from 'next/image'
+import { useMediaQuery } from 'react-responsive'
 
 const HomePage = () => {
-  return (
-    <main className="grid h-screen grid-rows-2 justify-center gap-2 ">
-      <Hero />
-      <div>
-        <Card>
-          <CardHeader>Login</CardHeader>
-          <CardContent>
-            <LoginButton>
-              <Button variant="default">Login</Button>
-            </LoginButton>
-          </CardContent>
-        </Card>
-      </div>
-    </main>
-  )
-}
+  const isMobile = useMediaQuery({ query: '(min-width: 640px)' })
 
-const Hero = () => {
   return (
-    <div className="flex flex-col justify-center">
-      <div className="flex flex-col items-center gap-2 md:flex-row lg:flex-row">
-        <div className="flex justify-center">
-          <Image
-            src={shieldIcon}
-            alt="Shield Icon"
-            className="max-w-12 md:mb-6 md:max-w-16 lg:mb-6 lg:max-w-20"
-          />
-        </div>
-        <div className="space-y-2 p-2">
-          <h1 className="text-center text-4xl md:text-6xl lg:text-7xl">
-            Next-Simple Auth
-          </h1>
+    <main className="grid min-h-screen items-center justify-center gap-2">
+      <div className="flex flex-col items-center gap-2 p-5 md:flex-row lg:flex-row">
+        <div className="flex flex-col items-center space-y-3 p-2 lg:space-y-5">
+          <div className="hero-shadow flex flex-col items-center gap-2 md:flex-row lg:flex-row">
+            <div className="max-w-12 md:max-w-16 lg:max-w-16">
+              <Image src={shieldIcon} alt="Shie1ld icon" />
+            </div>
+            <h1 className="text-center text-4xl font-semibold md:text-6xl lg:text-7xl">
+              Next-Simple Auth
+            </h1>
+          </div>
           <div>
-            <p className="text-center text-xs text-gray-300 md:text-lg lg:text-lg">
-              Authentication made easy.
+            <p className="text-center text-lg font-semibold text-gray-300 md:text-2xl lg:text-2xl">
+              Authentication made easy
             </p>
+          </div>
+          <div className="py-1">
+            <Button
+              className="hover:bg-hover rounded-full bg-primary font-semibold text-white"
+              size={!isMobile ? 'sm' : 'lg'}
+            >
+              Get Started
+            </Button>
           </div>
         </div>
       </div>
-    </div>
+    </main>
   )
 }
 
