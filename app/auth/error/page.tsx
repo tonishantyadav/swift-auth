1159cd12163bcb1a55bd5f774e7494967dd22e94
Nishant Yadav <inishantyadav24@gmail.com>
2024-04-24 00:27:02 +0530

    Refactor error page

--- app/auth/error/page.tsx
+++ app/auth/error/page.tsx
@@ -2,21 +2,10 @@
 
 import { Button } from '@/components/ui'
 import { Card, CardContent, CardFooter, CardHeader } from '@/components/ui/card'
-import { useRouter, useSearchParams } from 'next/navigation'
-import { useEffect, useState } from 'react'
+import { useRouter } from 'next/navigation'
 
 const AuthErrorPage = () => {
   const router = useRouter()
-  const searchParams = useSearchParams()
-  const [error, setError] = useState('')
-
-  useEffect(() => {
-    if (searchParams.get('error') === 'OAuthAccountNotLinked') {
-      setError('Email is already in use with different provider.')
-    } else if (searchParams.get('error')) {
-      setError('An unexpected error occurred.')
-    }
-  }, [searchParams])
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-[radial-gradient(#ffffff33_1px,#000000_1px)] bg-[size:20px_20px]">
@@ -24,11 +13,7 @@ const AuthErrorPage = () => {
         <CardHeader>
           <p className="text-4xl font-semibold">OAuth Error</p>
         </CardHeader>
-        {error && (
-          <CardContent>
-            <p>{error}</p>
-          </CardContent>
-        )}
+        <CardContent>An unexpected error occurred.</CardContent>
         <CardFooter>
           <Button
             className="w-full text-lg font-semibold transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110"
